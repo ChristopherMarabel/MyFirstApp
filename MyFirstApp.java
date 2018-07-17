1819e6fb9748f60195559e9047fe0e273614ea24
INDRA\cmarabel <cmarabelcalderon@gmail.com>
2018-07-17 08:31:26 +0200

    MyFirstApp

---
+++ MyFirstApp.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author cmarabel
+ *
+ */
+public class MyFirstApp {
+
+	public static void main(String[] args) {
+		System.out.println("I Rule!");
+		System.out.println("The World!");
+		int x = 4; // assign 4 to x
+		while (x > 3) {
+		// loop code will run because
+		// x is greater than 3
+		x = x-1; // or we'd loop forever
+		}
+		int z = 27; //
+		while (z == 17);
+		// loop code will not run because
+		// z is not equal to 17	
+		System.out.println(x);
+
+	}
+
+}
